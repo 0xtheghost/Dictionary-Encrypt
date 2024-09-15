b4f0ff4b599f06903b8b99543262d382e4698250
The Ghost <mael.demons17@gmail.com>
2024-09-15 13:21:57 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+from colorama import *
+
+banner = """
+  _                                   
+ / \   _|_ |_   _   _  |_   _   _ _|_ 
+ \_/ >< |_ | | (/_ (_| | | (_) _>  |_ 
+                    _|                               
+"""
+
+dico = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+
+def main(alphabet):
+    print(Fore.LIGHTRED_EX)
+    print(banner)
+    alpha = alphabet.copy()
+    random.shuffle(alpha)
+    print(Fore.BLUE)
+    print("Dict encrypt : ")
+    print({alphabet[i]: alpha[i] for i in range(len(alphabet))})
+
+main(dico)
+print(Fore.RESET)
